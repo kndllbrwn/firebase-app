542e6a20f996cbd194403c764d442a1e8e04093d
Kendall Brown <kndllbrwn1914@gmail.com>
2020-06-24 21:44:29 -0400

    first commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is body",
+      createdAt: "2020-06-23T01:02:25.906Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
